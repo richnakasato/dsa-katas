d39cea79d2ff3e170163e1dfd29e7289a5b6b5f5
Rich Nakasato <richnakasato@hotmail.com>
2019-01-10 16:38:57 -0500

    implemented, needed to lookup QS again

--- MergeSort_004.py
+++ MergeSort_004.py
@@ -1,7 +1,39 @@
 import random
 
-def mergesort():
-    pass
+def merge(left, right):
+    len_left = len(left)
+    len_right = len(right)
+    merged = [None] * (len_left + len_right)
+    i = j = k = 0
+    while i < len_left and j < len_right:
+        if left[i] < right[j]:
+            merged[k] = left[i]
+            i+=1
+        else:
+            merged[k] = right[j]
+            j+=1
+        k+=1
+    while i < len_left:
+        merged[k] = left[i]
+        i+=1
+        k+=1
+    while j < len_right:
+        merged[k] = right[j]
+        j+=1
+        k+=1
+    return merged
+
+
+def mergesort(arr):
+    len_arr = len(arr)
+    if len_arr < 2:
+        return arr
+    else:
+        mid = len_arr//2
+        left = mergesort(arr[:mid])
+        right = mergesort(arr[mid:])
+        merged = merge(left, right)
+        return merged
 
 
 def main():
@@ -10,7 +42,7 @@ def main():
     n = 10
     arr = [random.randint(lo, hi) for _ in range(n)]
     arr_sort = sorted(arr[:])
-    assert mergesort()==arr_sort
+    assert mergesort(arr)==arr_sort
 
 if __name__ == "__main__":
     main()
---
+++ QuickSort_005.py
@@ -0,0 +1,32 @@
+import random
+
+def partition(arr, start, end):
+    pivot = arr[end]
+    p_idx = start
+    for i in range(start, end):
+        if arr[i] < pivot:
+            arr[i], arr[p_idx] = arr[p_idx], arr[i]
+            p_idx += 1
+    arr[end], arr[p_idx] = arr[p_idx], arr[end]
+    return p_idx
+
+
+def quicksort(arr, start, end):
+    if start < end:
+        p_idx = partition(arr, start, end)
+        quicksort(arr, start, p_idx-1)
+        quicksort(arr, p_idx+1, end)
+
+
+def main():
+    lo = 0
+    hi = 100
+    n = 10
+    arr = [random.randint(lo, hi) for _ in range(n)]
+    arr_sort = sorted(arr[:])
+    quicksort(arr, 0, len(arr)-1)
+    assert arr==arr_sort
+
+if __name__ == "__main__":
+    main()
+
